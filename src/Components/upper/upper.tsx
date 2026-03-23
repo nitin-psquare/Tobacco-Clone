2b1c0e5c28833c89cfaf34200f1c1729ec6b19c5
Avantik Thakur <avantik.thakur@psquarecompany.com>
2026-03-23 11:37:46 +0530

    show and unshow of text

--- src/Components/upper/upper.tsx
+++ src/Components/upper/upper.tsx
@@ -1,20 +1,24 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "./upper.css";
 import gsap from "gsap";
 
 const Upper = () => {
   const imageRef = useRef<HTMLImageElement>(null);
   const textRef = useRef<HTMLDivElement>(null);
+  const outlintTextRef = useRef<HTMLHeadingElement>(null);
+
+  const [show, setShow] = useState(false);
 
   const handleMouseEnter = (e: React.MouseEvent) => {
+    if (show) return;
     gsap.set([imageRef.current, textRef.current], {
       WebkitMaskImage: `radial-gradient(circle 0px at ${e.clientX}px ${e.clientY}px, black 40%, transparent 100%)`,
       maskImage: `radial-gradient(circle 0px at ${e.clientX}px ${e.clientY}px, black 40%, transparent 100%)`,
     });
 
     gsap.to([imageRef.current, textRef.current], {
-      WebkitMaskImage: `radial-gradient(circle 300px at ${e.clientX}px ${e.clientY}px, black 40%, transparent 100%)`,
-      maskImage: `radial-gradient(circle 300px at ${e.clientX}px ${e.clientY}px, black 40%, transparent 100%)`,
+      WebkitMaskImage: `radial-gradient(circle 250px at ${e.clientX}px ${e.clientY}px, black 40%, transparent 100%)`,
+      maskImage: `radial-gradient(circle 250px at ${e.clientX}px ${e.clientY}px, black 40%, transparent 100%)`,
       duration: 0.5,
       ease: "power2.out",
       overwrite: "auto",
@@ -22,6 +26,7 @@ const Upper = () => {
   };
 
   const handleMouseMove = (e: React.MouseEvent) => {
+    if (show) return;
     gsap.to([imageRef.current, textRef.current], {
       WebkitMaskImage: `radial-gradient(circle 250px at ${e.clientX}px ${e.clientY}px, black 40%, transparent 100%)`,
       maskImage: `radial-gradient(circle 250px at ${e.clientX}px ${e.clientY}px, black 40%, transparent 100%)`,
@@ -32,13 +37,54 @@ const Upper = () => {
   };
 
   const handleMouseLeave = (e: React.MouseEvent) => {
+    if (show) return;
     gsap.to([imageRef.current, textRef.current], {
       WebkitMaskImage: `radial-gradient(circle 0px at ${e.clientX}px ${e.clientY}px, black 40%, transparent 100%)`,
       maskImage: `radial-gradient(circle 0px at ${e.clientX}px ${e.clientY}px, black 40%, transparent 100%)`,
-      duration: 0.5,
+      duration: 0,
       ease: "power2.out",
       overwrite: "auto",
     });
+  };
+
+  const handleClick = (e: React.MouseEvent) => {
+    const reveal = !show;
+    setShow(reveal);
+
+    if (reveal) {
+      gsap.to([outlintTextRef.current, textRef.current], {
+        opacity: 0,
+        y: -150,
+        duration: 0.5,
+        ease: "power2.out",
+        overwrite: "auto",
+      });
+
+      gsap.to([imageRef.current, textRef.current], {
+        WebkitMaskImage: `radial-gradient(circle 3000px at ${e.clientX}px ${e.clientY}px, black 40%, transparent 100%)`,
+        maskImage: `radial-gradient(circle 3000px at ${e.clientX}px ${e.clientY}px, black 40%, transparent 100%)`,
+        duration: 2,
+        ease: "power2.out",
+        overwrite: "auto",
+      });
+    } else {
+      gsap.to([outlintTextRef.current, textRef.current], {
+        opacity: 1,
+        y: 0,
+        duration: 2,
+        delay: 1,
+        ease: "power3.out",
+        overwrite: "auto",
+      });
+
+      gsap.to([imageRef.current, textRef.current], {
+        WebkitMaskImage: `radial-gradient(circle 200px at ${e.clientX}px ${e.clientY}px, black 40%, transparent 100%)`,
+        maskImage: `radial-gradient(circle 200px at ${e.clientX}px ${e.clientY}px, black 40%, transparent 100%)`,
+        duration: 3,
+        ease: "power2.out",
+        overwrite: "auto",
+      });
+    }
   };
 
   return (
@@ -47,13 +93,14 @@ const Upper = () => {
       onMouseEnter={handleMouseEnter}
       onMouseMove={handleMouseMove}
       onMouseLeave={handleMouseLeave}
+      onClick={handleClick}
     >
       <img
         ref={imageRef}
         src="https://images.unsplash.com/photo-1773402269635-3484c42a7860?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt=""
       />
-      <h1 className="text">
+      <h1 className="text" ref={outlintTextRef}>
         TOBACCO <br /> THEATRE
       </h1>
       <div className="text-mask-container" ref={textRef}>
