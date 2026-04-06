67f06754a5c7ba5b4b719997addc62a5ef554862
aman <aman.panchal@psquarecompany.com>
2026-04-06 13:17:57 +0530

    Added the BG color animation

--- src/Components/VenueRental/Middle/RoomList.tsx
+++ src/Components/VenueRental/Middle/RoomList.tsx
@@ -1,14 +1,61 @@
-import type { Dispatch } from "react";
+import type { Dispatch, MouseEvent } from "react";
 import "./RoomList.css"
+import gsap from "gsap";
 const RoomList = ({sethoveredRoom, img} : {sethoveredRoom: Dispatch<React.SetStateAction<{previous: string | null; current:string | null}>>, img: string} ) => {
   return (
     <div className="v-r-room"
-        
-    onMouseEnter={() => {
+
+    onMouseLeave={(e: MouseEvent<HTMLDivElement>) => {
+
+        const parent = e.currentTarget.getBoundingClientRect() ;
+        const overlay = e.currentTarget.querySelector(".overlay") ;
+        const fromTop = (e.clientY - parent.top < parent.height/ 2) ;
+        // gsap.set(overlay, {
+        //         y: "0%" 
+        // })
+        // gsap.to(overlay, {
+        //         y: fromTop ? "-100%": "100%" 
+        // })
+        gsap.fromTo(overlay, {
+         y: gsap.getProperty(overlay, "y") ,
+        }, {
+                y: fromTop ? "-100%": "100%" 
+        })
+    }}
+
+
+
+    onMouseEnter={(e:MouseEvent<HTMLDivElement>) => {
         sethoveredRoom((prev)=>({
                 previous: prev?.current ?? null,
                 current: img
         }))
+        const parent = e.currentTarget.getBoundingClientRect() ;
+         const overlay = e.currentTarget.querySelector(".overlay") ;
+
+        const fromTop = (e.clientY - parent.top < parent.height/ 2) ;
+
+        // gsap.set(overlay, {
+        //         y: fromTop ? "-100%" : "100%"
+        // });
+
+        // gsap.to(overlay, {
+        //         y: "0%", 
+        //         duration: 0.8, 
+        //         ease:"power1"
+        // })
+
+
+        gsap.fromTo(overlay, {
+         y: gsap.getProperty(overlay, "y") ,
+        }, {
+                y: "0%", 
+                duration: 0.8, 
+                ease: "power1" 
+        })
+
+
+
     }}
     
     >
@@ -21,6 +68,10 @@ const RoomList = ({sethoveredRoom, img} : {sethoveredRoom: Dispatch<React.SetSta
         </div>
         <div className="room-area">
                 400m2
+        </div>
+
+        <div className="overlay">
+
         </div>
 
 
