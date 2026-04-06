4ab18876b049c1241036cb13d9eef34a6183171f
aman <aman.panchal@psquarecompany.com>
2026-04-06 15:12:38 +0530

    Merged

--- src/Components/VenueRental/VenueRental.tsx
+++ src/Components/VenueRental/VenueRental.tsx
@@ -1,40 +1,19 @@
-<<<<<<< Updated upstream
 import React from 'react'
 import MIddle from './Middle/MIddle'
-=======
 import Curtain from "../../common/curtain";
 import Upper from "./upper/upper";
->>>>>>> Stashed changes
 
 const VenueRental = () => {
   return (
     <div>
-<<<<<<< Updated upstream
-        <div className="venue-rental-upper">
-            hey 
-        </div>
-        <div className="venue-rental-middle">
-            
-          <MIddle/>
-
-
-
-        </div>
-        <div className="venue-rental-lower">
-
-        </div>
-
-
-
-
-=======
       <Curtain />
       <div className="venue-rental-upper">
         <Upper />
       </div>
-      <div className="venue-rental-middle"></div>
+      <div className="venue-rental-middle">
+        <MIddle />
+      </div>
       <div className="venue-rental-lower"></div>
->>>>>>> Stashed changes
     </div>
   );
 };
